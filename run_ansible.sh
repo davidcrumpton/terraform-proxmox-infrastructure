8a30402997e89e298dbeb29127c5b760ba043edc
bear <bear@crumpton.org>
2025-11-11 17:41:09 -0700

    cleaned up

--- run_ansible.sh
+++ run_ansible.sh
@@ -9,7 +9,7 @@ FIRST_HOST=$(grep -m1 -A1 "^\[all\]" "$INVENTORY" | tail -n1)
 echo "⏳ Waiting for $FIRST_HOST to be reachable via SSH..."
 for i in {1..10}; do
 #    if ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 "$FIRST_HOST" 'exit' 2>/dev/null; then
-    if nc -vw 5 atom5g-ng 22; then
+    if nc -vw 5 "$FIRST_HOST" 22; then
         echo "✅ $FIRST_HOST is reachable."
         break
     fi
@@ -19,6 +19,7 @@ done
 
 echo "🚀 Running playbook: $PLAYBOOK"
 
+export ANSIBLE_HOST_KEY_CHECKING=False
 
 # Setup SSH key if provided
 if [[ -n "${SSH_PRIVATE_KEY_BASE_64:-}" ]]; then
