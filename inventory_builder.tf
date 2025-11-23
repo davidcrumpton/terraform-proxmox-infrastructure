a3d34e1e28c18ef9752c2b64e7b55afa8fc10c64
bear <bear@crumpton.org>
2025-11-23 10:54:29 -0700

    enable nextsuite

--- inventory_builder.tf
+++ inventory_builder.tf
@@ -7,7 +7,7 @@ locals {
 
     # Add more here, e.g.:
 #    module.lxc_gl_runner.ansible_data,
-#    module.lxc_nextsuite.ansible_data,
+    module.lxc_nextsuite.ansible_data,
     module.lxc_wikijs.ansible_data,
 #    module.lxc_docker02.ansible_data,
     # module.lxc_keycloak.ansible_data,
